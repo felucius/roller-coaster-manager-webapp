236a49f2fd436eb83effac0fc90ce382673d65a2
felucius <maxime.de.lange@outlook.com>
2026-02-18 11:53:51 +0100

    Added ability to view the different employees in the employee screen

--- components/screens/EmployeesScreen.tsx
+++ components/screens/EmployeesScreen.tsx
@@ -1,10 +1,41 @@
 "use client";
+import { getEmployees } from "@/features/employees/logic";
+import { getTechnicianEmployees } from "@/features/employees/logic";
+import { getServerEmployees } from "@/features/employees/logic";
 
 export default function EmployeesScreen() {
     return (
         <main>
             <h1>Employees</h1>
             <p>Manage employees in the park.</p>
+
+            <h2>All current employed employees</h2>
+            <ul>
+                {getEmployees().map(employee => (
+                    <li key={employee.id}>
+                        {employee.name} - {employee.position} - ${employee.salary}
+                    </li>
+                ))}
+            </ul>
+
+            
+            <h2>Hire new technician</h2>
+            <ul>
+                {getTechnicianEmployees().map(employee => (
+                    <li key={employee.id}>
+                        {employee.name} - {employee.position} - ${employee.salary}
+                    </li>
+                ))}
+            </ul>
+
+            <h2>Hire new server</h2>
+            <ul>
+                {getServerEmployees().map(employee => (
+                    <li key={employee.id}>
+                        {employee.name} - {employee.position} - ${employee.salary}
+                    </li>
+                ))}
+            </ul>
         </main>
     );
 }
