da44c3c0f25ed2832c6de15b844589c8796b7dc5
felucius <maxime.de.lange@outlook.com>
2026-02-18 12:01:02 +0100

    added the customers list to the screen to it is visible

--- components/screens/CustomersScreen.tsx
+++ components/screens/CustomersScreen.tsx
@@ -1,10 +1,19 @@
 "use client";
+import { getCustomers } from "@/features/customers/logic";
 
 export default function CustomersScreen() {
     return (
         <main>
             <h1>Customers</h1>
             <p>Manage customers in the park.</p>
+
+            <ul>
+                {getCustomers().map((customer) => (
+                    <li key={customer.id}>
+                        {customer.name} - money: ${customer.money}
+                    </li>
+                ))}
+            </ul>
         </main>
     );
 }
