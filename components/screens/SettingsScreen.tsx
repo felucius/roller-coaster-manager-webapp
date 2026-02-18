dc5536d6cfc476c1acec42cf1ee60c3952f776a7
felucius <maxime.de.lange@outlook.com>
2026-02-18 13:34:34 +0100

    Added different button for future settings

--- components/screens/SettingsScreen.tsx
+++ components/screens/SettingsScreen.tsx
@@ -5,6 +5,30 @@ export default function SettingsScreen() {
         <main>
             <h1>Settings</h1>
             <p>Manage your park settings here.</p>
+
+            <div>
+                <button className="cheats">Enter Cheats</button>
+            </div>
+
+            <div>
+                <button className="save">Save game to file</button>
+            </div>
+
+            <div>
+                <button className="save to local storage">Save game to local storage</button>
+            </div>
+
+            <div>
+                <button className="load">Load game from file</button>
+            </div>
+
+            <div>
+                <button className="load from local storage">Load game from local storage</button>
+            </div>
+
+            <div>
+                <button className="reset">Reset game</button>
+            </div>
         </main>
     );
 }
